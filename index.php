9d30c7bea03df5a20103cc40f226dd535627a274
daisuke <>
2018-08-30 08:23:31 +0900

    firstcommit

---
+++ index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>本づくりを通して楽しい明日をつくる｜株式会社八鳥（ハチドリ） | 株式会社 八鳥（ハチドリ）</title>
+    <link rel="stylesheet" href="http://hatidori.jp/wp/wp-content/themes/hatidori/css/normalize.css">
+    <link rel="stylesheet" href="http://hatidori.jp/wp/wp-content/themes/hatidori/style.css">
+    <link rel="icon" href="http://hatidori.jp/wp/wp-content/themes/hatidori/images/favicon.ico" type="image/vnd.microsoft.icon">
+    <link href="https://fonts.googleapis.com/earlyaccess/sawarabimincho.css" rel="stylesheet" />
+    <link rel="stylesheet" href="/css/glide.core.min.css">
+    <link rel="stylesheet" href="/css/glide.theme.min.css">
+    <script src="/js/glide.js"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<script src="//api.html5media.info/1.1.8/html5media.min.js"></script>
+<![endif]-->
+    <!-- All in One SEO Pack 2.3.11 by Michael Torbert of Semper Fi Web Design[73,110] -->
+    <meta name="description" content="八鳥（ハチドリ）は、自分で本をつくりたいという方の出版をお手伝いしています。企画から出版まで、一貫してサポートするとともに、より多くの方の下へ届くよう日本中を飛び回ります。" />
+    <link rel='next' href='http://hatidori.jp/page/2/' />
+    <link rel="canonical" href="http://hatidori.jp/" />
+    <meta property="og:title" content="本づくりを通して楽しい明日をつくる｜株式会社八鳥（ハチドリ）" />
+    <meta property="og:url" content="http://hatidori.jp/" />
+    <meta property="og:image" content="http://hatidori.jp/wp/wp-content/uploads/2016/11/photo02.jpg" />
+    <meta property="og:site_name" content="株式会社八鳥（ハチドリ）" />
+    <meta property="og:description" content="八鳥（ハチドリ）は、自分で本をつくりたいという方の出版をお手伝いしています。企画から出版まで、一貫してサポートするとともに、より多くの方の下へ届くよう日本中を飛び回ります。" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="本づくりを通して楽しい明日をつくる｜株式会社八鳥（ハチドリ）" />
+    <meta name="twitter:description" content="八鳥（ハチドリ）は、自分で本をつくりたいという方の出版をお手伝いしています。企画から出版まで、一貫してサポートするとともに、より多くの方の下へ届くよう日本中を飛び回ります。" />
+    <meta name="twitter:image" content="http://hatidori.jp/wp/wp-content/uploads/2016/11/photo02.jpg" />
+    <meta itemprop="image" content="http://hatidori.jp/wp/wp-content/uploads/2016/11/photo02.jpg" />
+    <meta data-brackets-id='7' name="viewport" content="width=device-width, initial-scale=1">
+    <!-- /all in one seo pack -->
+    <link rel='dns-prefetch' href='//s.w.org' />
+    <link rel='stylesheet' id='contact-form-7-css' href='http://hatidori.jp/wp/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=4.5.1' type='text/css' media='all' />
+    <!-- top用新規css -->
+    <link rel="stylesheet" href="/css/top.css">
+    <link rel="stylesheet" media="(max-width: 480px)" href="/css/top_sp.css">
+
+    <!-- rwd設定 -->
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <section class="header_inner">
+                <h1>
+                    <a href=""><img src="/images/top/logo_top.png" width="147" height="35" alt="八鳥"></a>
+                </h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="">本づくり</a>
+                        </li>
+                        <li>
+                            <a href="">八鳥とは</a>
+                        </li>
+                        <li>
+                            <a href="">ブログ</a>
+                        </li>
+                        <li>
+                            <a href="">ショップ</a>
+                        </li>
+                        <li>
+                            <a href="">お問い合わせ</a>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+        </header>
+        <!-- end header-->
+        <section id="contents" class="container clearfix nopadding">
+            <main>
+                <article>
+                    <section class="books">
+                        <h2>本のご紹介</h2>
+                        <p class="sub_title">本のわくから飛び出した本のかたち</p>
+                        <div class="cover">
+                            <div class="glide" data-component="hero">
+                                <div data-glide-el="track" class="glide__track">
+                                    <ul class="glide__slides">
+                                        <li class="glide__slide">
+                                            <a href="http://shop.hatidori.jp/items/7555208"><img src="/images/top/slider01.png" alt="" data-ref="hero[el]" width="250" height="350"></a>
+                                        </li>
+                                        <li class="glide__slide">
+                                            <a href="http://shop.hatidori.jp/items/8756739"><img src="/images/top/slider02.png" alt="" data-ref="hero[el]" width="290"></a>
+                                        </li>
+                                        <li class="glide__slide">
+                                            <a href="http://shop.hatidori.jp/items/5189875"><img src="/images/top/slider03.png" alt="" data-ref="hero[el]" width="250" height="350"></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- CSS -->
+                        <style>
+                            /* 傾きを表示するため、各要素のoveflow:hiddenをオーバーライド */
+                            
+                            .glide__track,
+                            .glide__slides {
+                                overflow: visible;
+                            }
+                            /* 動きをなめらかに */
+                            
+                            .glide__slide img {
+                                transition: transform 1s ease-in-out;
+                                transition: transform 1s ease-in-out, -webkit-transform 1s ease-in-out;
+                                will-change: transform;
+                            }
+
+                        </style>
+
+                        <!-- JS -->
+                        <script>
+                            window.onload = function() {
+                                // 傾きをもたせる対象の指定
+                                var targetImgs = '[data-ref="hero[el]"]';
+                                new Glide('.glide', {
+                                    // オプション
+                                    focusAt: "center",
+                                    startAt: 1,
+                                    autoplay: 350000,
+                                    animationDuration: 1e3,
+                                    rewindDuration: 2e3,
+                                    touchRatio: .25,
+                                    perTouch: 1
+                                }).mount({
+                                    // 傾き制御
+                                    Coverflow: function(glideWrap, setting, glide) {
+                                        var tiltFunc = {
+                                            // カレント制御・各処理の呼び出し
+                                            tilt: function(activeList) {
+                                                // カレント制御
+                                                activeList.querySelector(targetImgs).style.transform = "perspective(700px) rotateY(0deg)";
+                                                $('.next_1').remove();
+                                                $('.next_2').remove();
+                                                $('.pre_1').remove();
+                                                $('.pre_2').remove();
+
+
+                                                // 前・次の要素の傾き制御
+                                                this.tiltPrevElements(activeList);
+                                                this.tiltNextElements(activeList);
+                                            },
+                                            // 前の要素の制御
+                                            tiltPrevElements: function(activeList) {
+                                                for (var slideList = function(activeList) {
+                                                        var slideList = [];
+                                                        if (activeList) {
+                                                            for (; activeList = activeList.previousElementSibling;) {
+                                                                slideList.push(activeList);
+                                                            }
+                                                        }
+                                                        return slideList
+                                                    }(activeList), cnt = 0; cnt < slideList.length; cnt++) {
+                                                    var prevImg = slideList[cnt].querySelector(targetImgs);
+                                                    prevImg.style.transformOrigin = "100% 50%";
+                                                    prevImg.style.transform = "perspective(700px) rotateY(30deg) skewY(10deg) scaleX(0.9)";
+                                                    prevImg.insertAdjacentHTML('afterend', '<div class="pre_1"></div><div class="pre_2"></div>');
+                                                    var img_width = prevImg.width;
+                                                    var img_height = prevImg.height;
+                                                    var pre_1_wh = document.getElementsByClassName("pre_1");
+                                                    pre_1_wh[0].style.width = img_width + "px";
+                                                    pre_1_wh[0].style.height = img_height + "px";
+                                                    var pre_2_wh = document.getElementsByClassName("pre_2");
+                                                    pre_2_wh[0].style.width = img_width + "px";
+                                                    pre_2_wh[0].style.height = img_height + "px";
+
+                                                }
+                                            },
+                                            // 次の要素の制御
+                                            tiltNextElements: function(activeList) {
+                                                for (var slideList = function(activeList) {
+                                                        var slideList = [];
+                                                        if (activeList) {
+                                                            for (; activeList = activeList.nextElementSibling;) {
+                                                                slideList.push(activeList);
+                                                            }
+                                                        }
+                                                        return slideList;
+                                                    }(activeList), cnt = 0; cnt < slideList.length; cnt++) {
+                                                    var nextImg = slideList[cnt].querySelector(targetImgs);
+                                                    nextImg.style.transformOrigin = "0% 50%";
+                                                    nextImg.style.transform = "perspective(700px) rotateY(-30deg) skewY(-10deg) scaleX(0.9)";
+                                                    nextImg.insertAdjacentHTML('afterend', '<div class="next_1"></div><div class="next_2"></div>');
+                                                    var img_width = nextImg.width;
+                                                    var img_height = nextImg.height;
+                                                    var next_1_wh = document.getElementsByClassName("next_1");
+                                                    next_1_wh[0].style.width = img_width + "px";
+                                                    next_1_wh[0].style.height = img_height + "px";
+                                                    var next_2_wh = document.getElementsByClassName("next_2");
+                                                    next_2_wh[0].style.width = img_width + "px";
+                                                    next_2_wh[0].style.height = img_height + "px";
+
+
+                                                    console.log(img_width);
+                                                    console.log(img_height);
+
+                                                }
+                                            }
+                                        };
+                                        return glide.on(["mount.after", "run"], function() {
+                                            tiltFunc.tilt(setting.Html.slides[glideWrap.index])
+                                        }), tiltFunc
+                                    }
+                                });
+                            }
+
+                        </script>
+                    </section>
+                    <section>
+                        <h2>作家のご紹介</h2>
+                        <p class="sub_title">個性あふれる作家さんたちの活動や本のつくり方
+                        </p>
+                        <div class="column_box">
+                            <div class="column_3">
+                                <img src="/images/top/author01.png" alt="写真">
+                                <div class="text_box">
+                                    <p class="name">水上 庭</p>
+                                    <p class="text-center">舞台俳優／小説家</p>
+                                    <p>東京芸術大学美術学部先端芸術表現科卒。俳優として活動する傍ら、桐沢たえ名義で同人誌小説を執筆。<br>舞台の脚本のような視覚化しやすい表現と繊細な心の変化を捉えた描写に定評がある。
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="column_3">
+                                <img src="/images/top/author02.png" alt="写真">
+                                <div class="text_box">
+                                    <p class="name">joh</p>
+                                    <p class="text-center">絵描き</p>
+                                    <p>東京と名古屋を中心に活動中。暮らしのなかにある気づきをコンセプトにした作品をつくる。<br>ショッピングモールでのライブペイントや子どものための絵画教室などを通して、絵を描くことの楽しさも伝える。
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="column_3">
+                                <img src="/images/top/author03.png" alt="写真">
+                                <div class="text_box">
+                                    <p class="name">melancolia storytelling<br>（相澤 歩）</p>
+                                    <p class="text-center">シンガーソングライター</p>
+                                    <p> 見たことはないけれどよく知ってるような景色をテーマにしたうたと物語と写真と映像と庭をつくる。<br>Twitterとinstagramでオリジナルストーリーをほぼ毎日配信中。
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <section>
+                        <h2>ブログ</h2>
+                        <p class="sub_title">日々の活動や出版情報など</p>
+                        <div id="media_list">
+                            <ul class="clearfix column_box">
+                                <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+                                <li class="column_4">
+                                    <a href="<?php the_permalink();?>">
+                                        <?php if(in_category('1')): ?>
+                                        <div class="corner blue">
+                                            <?php elseif(in_category('2')): ?>
+                                            <div class="corner red">
+                                                <?php elseif(in_category('3')): ?>
+                                                <div class="corner yellow">
+                                                    <?php elseif(in_category('4')): ?>
+                                                    <div class="corner green">
+                                                        <?php else: ?>
+                                                        <div class="corner blue">
+                                                            <?php endif; ?>
+                                                            <div class="media_image">
+                                                                <?php $title = get_the_title(); the_post_thumbnail('single-thumbnail', array( 'class' => 'img-responsive', 'alt' => $title )); ?>
+                                                            </div>
+                                                        </div>
+                                                        <div class="media_info">
+                                                            <h2>
+                                                                <?php the_title(); ?>
+                                                            </h2>
+                                                        </div>
+                                    </a>
+                                </li>
+                                <?php endwhile; // end of the loop. ?>
+                            </ul>
+                    </section>
+                    <?php wp_pagenavi(); ?>
+                </article>
+            </main>
+            <!-- end main-->
+            <?php include( TEMPLATEPATH . '/content/aside.php' ); ?>
+        </section>
+        <!-- end #contents-->
+        <?php get_footer(); ?>
